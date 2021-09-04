c8b6a2bcefaba6244307ae5d4cb8238fd779f7fc
ruben cardoza <rubencardozajr@unimagdalenma.edu.co>
2021-09-04 15:11:12 -0500

    configuracion de lptmr0 y tambien la cracion de las librerias

--- K32L2B31A_Project/board/peripherals.c
+++ K32L2B31A_Project/board/peripherals.c
@@ -60,7 +60,8 @@ instance:
 - peripheral: 'NVIC'
 - config_sets:
   - nvic:
-    - interrupt_table: []
+    - interrupt_table:
+      - 0: []
     - interrupts: []
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS **********/
 /* clang-format on */
@@ -152,12 +153,68 @@ static void ADC0_init(void) {
 }
 
 /***********************************************************************************************************************
+ * LPTMR0 initialization code
+ **********************************************************************************************************************/
+/* clang-format off */
+/* TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
+instance:
+- name: 'LPTMR0'
+- type: 'lptmr'
+- mode: 'LPTMR_GENERAL'
+- custom_name_enabled: 'false'
+- type_id: 'lptmr_48552e76e8733b28a9c768b6d8d4fefa'
+- functional_group: 'BOARD_InitPeripherals'
+- peripheral: 'LPTMR0'
+- config_sets:
+  - fsl_lptmr:
+    - lptmr_config:
+      - timerMode: 'kLPTMR_TimerModeTimeCounter'
+      - pinSelect: 'ALT.0'
+      - pinPolarity: 'kLPTMR_PinPolarityActiveHigh'
+      - enableFreeRunning: 'false'
+      - bypassPrescaler: 'true'
+      - prescalerClockSource: 'kLPTMR_PrescalerClock_1'
+      - clockSource: 'BOARD_BootClockRUN'
+      - value: 'kLPTMR_Prescale_Glitch_0'
+      - timerPeriod: '1s'
+    - enableInterrupt: 'true'
+    - interrupt:
+      - IRQn: 'LPTMR0_IRQn'
+      - enable_interrrupt: 'enabled'
+      - enable_priority: 'false'
+      - priority: '0'
+      - enable_custom_name: 'false'
+ * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS **********/
+/* clang-format on */
+const lptmr_config_t LPTMR0_config = {
+  .timerMode = kLPTMR_TimerModeTimeCounter,
+  .pinSelect = kLPTMR_PinSelectInput_0,
+  .pinPolarity = kLPTMR_PinPolarityActiveHigh,
+  .enableFreeRunning = false,
+  .bypassPrescaler = true,
+  .prescalerClockSource = kLPTMR_PrescalerClock_1,
+  .value = kLPTMR_Prescale_Glitch_0
+};
+
+static void LPTMR0_init(void) {
+  /* Initialize the LPTMR */
+  LPTMR_Init(LPTMR0_PERIPHERAL, &LPTMR0_config);
+  /* Set LPTMR period */
+  LPTMR_SetTimerPeriod(LPTMR0_PERIPHERAL, LPTMR0_TICKS);
+  /* Configure timer interrupt */
+  LPTMR_EnableInterrupts(LPTMR0_PERIPHERAL, kLPTMR_TimerInterruptEnable);
+  /* Enable interrupt LPTMR0_IRQn request in the NVIC. */
+  EnableIRQ(LPTMR0_IRQN);
+}
+
+/***********************************************************************************************************************
  * Initialization functions
  **********************************************************************************************************************/
 void BOARD_InitPeripherals(void)
 {
   /* Initialize components */
   ADC0_init();
+  LPTMR0_init();
 }
 
 /***********************************************************************************************************************
